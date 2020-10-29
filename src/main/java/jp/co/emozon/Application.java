6623d3fdd070e0bc709f9c22cf9fee5991b4645a
shuntae <utsuhoranka@gmail.com>
2020-10-30 00:16:21 +0900

    [emozon] DBの接続設定を追加（現在は接続先のDBが存在しないため、DB接続機能をOFFにしております）

--- src/main/java/jp/co/emozon/Application.java
+++ src/main/java/jp/co/emozon/Application.java
@@ -1,13 +1,19 @@
 package jp.co.emozon;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(Application.class, args);
 
 	}
